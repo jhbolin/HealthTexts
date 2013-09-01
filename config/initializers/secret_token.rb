986ea4a66b395f9a325c80cc8506c35c3549f459
Joshua Bolin <jhbolin@yahoo.com>
2013-09-01 19:20:11 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Healthtexts::Application.config.secret_token = '0bfbdfe477e0f4d2ca4aa186bb5c4b8b972dce57f6b6399836961d147e3fb0c99c140a0a4b1595ffb382e7479e7b1d07a4e1a0498161a0bb8799ec9b8157c321'
