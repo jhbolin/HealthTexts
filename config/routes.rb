568307d1281486b4cdf094b753b66653cf6d014d
Joshua Bolin <jhbolin@yahoo.com>
2013-09-03 00:32:01 -0400

    Bootstrap added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Healthtexts::Application.routes.draw do
+  get 'about' => 'pages#about'
+
+
+  root :to => 'pages#home'
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
